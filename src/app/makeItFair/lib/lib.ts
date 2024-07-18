587ee35e4a3a7f4603f081ebc06dc10b68e33a13
zeeyeeteecar <101447550+zeeyeeteecar@users.noreply.github.com>
2024-07-18 10:14:20 -0700

    2

--- src/app/makeItFair/lib/lib.ts
+++ src/app/makeItFair/lib/lib.ts
@@ -4,7 +4,7 @@ import { revalidatePath } from "next/cache";
 export async function fetch_answerList() {
   const prisma = new PrismaClient();
   const response = await prisma.tbl_MakeItFair_2024.findMany();
-  revalidatePath("/");
+  revalidatePath("makeItFair");
   return response;
 }
 
