8f4a93c2dbe700852c37e2517b88e80c5d5e962f
zeeyeeteecar <101447550+zeeyeeteecar@users.noreply.github.com>
2024-07-19 14:53:16 -0700

    2

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -6,8 +6,12 @@ export async function GET() {
       message: "Logout successful",
       success: true,
     });
-    response.cookies.set("token", "", { httpOnly: true, expires: new Date(0), maxAge: 0 });
-    
+
+    response.cookies.set("token", "", {
+      httpOnly: true,
+      expires: new Date(0),
+      maxAge: 0,
+    });
 
     return response;
   } catch (error) {
